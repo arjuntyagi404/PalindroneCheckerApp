577f7142bc17c4d941960934f7ddbe55fad0f07c
Nishant Ranjan <nishant.ranjan.air1@gmail.com>
2026-02-19 09:37:37 +0530

    added usecase2

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -9,3 +9,28 @@ public class PalindroneCheckerApp {
                 "\nSystem initialized successfully");
     }
 }
+
+
+ */
+
+public class PalindroneCheckerApp {
+
+    public static boolean isPalindrome(String str) {
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String word1 = "madam";
+        String word2 = "hello";
+
+        System.out.println(word1 + " is a palindrome: " + isPalindrome(word1));
+        System.out.println(word2 + " is a palindrome: " + isPalindrome(word2));
+    }
+}
